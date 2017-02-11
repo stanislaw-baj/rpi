dbcf2a6fbc3f881a6e27ac5d3e18fa5690dbb09f
macio <macie.baj@gmail.com>
2017-02-11 18:37:42 +0100

    Update raspberry-update.sh
    kill running python process

--- raspberry-update.sh
+++ raspberry-update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ssh pi@192.168.0.101 'cd ~/rpi && git pull'
+ssh pi@192.168.0.101 'cd ~/rpi && kill $(ps aux | grep \'python read_test.py\' | awk \'{print $2}\'); git pull; && nohup python read_test.py > read_log.log &' &
+
