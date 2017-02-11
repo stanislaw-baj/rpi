37b92f3fad1a54288b94150b61070ebdc3c46050
znt <znt@znt.com.pl>
2017-02-11 17:33:12 +0100

    add read and write files

---
+++ write_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+          
+      
+import time
+import serial
+          
+ser = serial.Serial(             
+        port='/dev/ttyS0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+      )
+counter=0
+          
+      
+while 1:
+        ser.write('Write counter: %d \n'%(counter))
+	print "Wysylam: " + str(counter)
+        time.sleep(1)
+        counter += 1
